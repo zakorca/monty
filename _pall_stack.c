3725081b25ae1fd7a8d6a765414187624e32ca0a
zakorca <zakariatouras47.com>
2023-12-14 17:41:36 -0800

    filesup

---
+++ _pall_stack.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+/**
+ * pall - print elements of stack from the top.
+ * @stack: the head
+ * @line_number: (void)
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+	(void)line_number;
+
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
