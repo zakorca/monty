68cc7fad3eb7ba1c23327a7ed12488ec301bde4c
zakorca <zakariatouras47.com>
2023-12-14 17:42:12 -0800

    filesup

---
+++ _pint.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * pint_func - code checking ..
+ *
+ * @stack: pointer to stack
+ * @line_num: line nmbr
+ *
+ * Return: void
+ */
+void pint_func(stack_t **stack, unsigned int line_num)
+{
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
