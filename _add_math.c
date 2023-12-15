13c30608be784c3377f329f5c08f037ad2975032
zakorca <zakariatouras47.com>
2023-12-14 17:38:29 -0800

    filesup

---
+++ _add_math.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * add_nodes - add 2 nodes & pop the last.
+ *
+ * @stack: head
+ * @line_add: line
+ */
+void add_nodes(stack_t **stack, uint line_add)
+{
+	int sum_node;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_add);
+		exit(EXIT_FAILURE);
+	}
+	sum_node = (*stack)->n + (*stack)->next->n;
+	pop_stack(stack, line_add);
+	(*stack)->n = sum_node;
+}
