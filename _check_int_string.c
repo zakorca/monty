e15d7b8b6e9766091fd0e9186d7522d71ad37a5b
zakorca <zakariatouras47.com>
2023-12-14 17:39:19 -0800

    filesup

---
+++ _check_int_string.c
@@ -0,0 +1,23 @@
+#include"monty.h"
+/**
+ * check_integer - check if a string is  integers
+ * @string: the string
+ *
+ * Return: return 0 or 1
+ */
+int check_integer(char *string)
+{
+	int i = 0;
+
+	if (string[0] == '-')
+		i++;
+	while (string[i] != '\0')
+	{
+		if (string[i] > '9' || string[i] < '0')
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
